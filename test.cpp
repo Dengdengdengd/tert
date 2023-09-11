8e54ef50b58f97aa89982c405e7f7b6ea5586035
Dengdengdengd <2975274140@qq.com>
2023-09-11 14:32:24 +0800

    master

--- test.cpp
+++ test.cpp
@@ -1,7 +1,64 @@
 #include<iostream>
 using namespace std;
+class Vec2{
+    private:
+        double u;
+        double v;
+    public:
+        Vec2(double u=0,double v=0);
+        double getU() const;
+        double getV() const;
+        Vec2 operator+(const Vec2&b);
+        friend Vec2 operator-(const Vec2&a,const Vec2&b);
+        bool operator==(const Vec2&b) const;
+        friend bool operator!=(const Vec2&a,const Vec2&b);
+        friend ostream&operator<<(ostream&os,const Vec2&c);
+        friend istream&operator>>(istream&is,Vec2&c);
+};
+double Vec2::getU() const
+{
+    return u;
+}
+double Vec2::getV() const
+{
+    return v;
+}
+Vec2 operator-(const Vec2&a,const Vec2&b){
+    return Vec2(a.u-b.u,a.v-b.v);
+}
+bool Vec2::operator==(const Vec2&b) const{
+    return u==b.u&&v==b.v;
+}
+
+Vec2::Vec2(double u, double v) : u(u), v(v) {}
+
+
+Vec2 Vec2::operator+(const Vec2&b){
+    return Vec2(u+b.u,v+b.v) ;
+}
+bool operator!=(const Vec2&a,const Vec2&b)
+{
+    return a.u!=b.u&&a.v!=b.v;
+}
+ostream&operator<<(ostream&os,const Vec2&c){
+   os <<"u="<< c.u <<",v="<< c.v;
+    return os;
+}
+istream&operator>>(istream&is,Vec2&c){
+   is >> c.u >> c.v;
+    return is;
+}
 
 int main(){
-    cout<<"hello world!!"<<endl;
-    cout<<"woshishuaige"<<endl;
+    Vec2 a;
+    cin>>a;
+    cout<<"a: "<<a<<endl;
+    Vec2 b(3,4);
+    Vec2 c=a+b;
+    cout<<"c: "<<c<<endl;
+    Vec2 d=a-b;
+    cout<<"d: "<<d<<endl;
+    cout<<"a==a: "<<(a==a)<<endl;
+    cout<<"a!=a: "<<(a!=a)<<endl;
+    return 0;
 }
